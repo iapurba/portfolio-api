be61980a22e320619dfadd70af3a06b005eeab87
Apurba Panja <apurbapanja1@gmail.com>
2024-02-25 23:24:52 +0530

    Initial setup

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('The Ultimate SDE Portfolio API')
+    .setDescription(
+      'This app provides RESTFul APIs for software developer portfolio',
+    )
+    .setVersion('1.0')
+    .addTag('profile')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
+}
+bootstrap();
