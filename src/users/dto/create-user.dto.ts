224b24c1ff1549ffd56ae002e55e5f70455fd78d
Apurba Panja <apurbapanja1@gmail.com>
2024-03-20 21:13:07 +0530

    Added Password Validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsEnum, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsEnum, IsNotEmpty, IsStrongPassword } from 'class-validator';
 import { UserRole } from '../enums/role.enum';
 
 export class CreateUserDto {
@@ -7,6 +7,7 @@ export class CreateUserDto {
   readonly email: string;
 
   @IsNotEmpty()
+  @IsStrongPassword()
   readonly password: string;
 
   @IsNotEmpty()
