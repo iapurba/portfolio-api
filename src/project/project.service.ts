b8beaef2a2a630c4df80aa0def1a0b7981185767
Apurba Panja <apurbapanja1@gmail.com>
2024-03-23 10:05:39 +0530

    Added Error handling

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,6 +1,7 @@
 import {
   BadRequestException,
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
@@ -21,68 +22,88 @@ export class ProjectService {
   ) {}
 
   private async findProfile(profileId: string): Promise<Profile> {
-    const profile = await this.profileService.getProfileById(profileId);
-    if (!profile) {
-      throw new BadRequestException(profileConstants.BAD_REQUEST);
+    try {
+      const profile = await this.profileService.getProfileById(profileId);
+      if (!profile) {
+        throw new BadRequestException(profileConstants.BAD_REQUEST);
+      }
+      return profile;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-    return profile;
   }
 
   async findProjectsByProfileId(profileId: string): Promise<Project[]> {
-    const profile = await this.findProfile(profileId);
-    const projects = await this.projectModel.find({ profileId: profile });
-    return projects ?? [];
+    try {
+      const profile = await this.findProfile(profileId);
+      const projects = await this.projectModel.find({ profileId: profile });
+      return projects ?? [];
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 
   async getProject(projectId: string): Promise<Project> {
-    const project = await this.projectModel.findById(projectId);
-    if (!project) {
-      throw new NotFoundException(projectConstants.NOT_FOUND);
+    try {
+      const project = await this.projectModel.findById(projectId);
+      if (!project) {
+        throw new NotFoundException(projectConstants.NOT_FOUND);
+      }
+      return project;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-    return project;
   }
 
   async createProjects(
     profileId: string,
     projectDataList: CreateProjectDto[],
   ): Promise<Project[]> {
-    const profile = await this.findProfile(profileId);
-    if (!profile) {
-      throw new NotFoundException(profileConstants.NOT_FOUND);
+    try {
+      const profile = await this.findProfile(profileId);
+      if (!profile) {
+        throw new NotFoundException(profileConstants.NOT_FOUND);
+      }
+      const createdProjects = await this.projectModel.insertMany(
+        projectDataList.map((data: CreateProjectDto) => ({
+          ...data,
+          profileId: profile._id,
+        })),
+      );
+      return createdProjects;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-    const createdProjects = await this.projectModel.insertMany(
-      projectDataList.map((data: CreateProjectDto) => ({
-        ...data,
-        profileId: profile._id,
-      })),
-    );
-    return createdProjects;
   }
 
   async updateProject(
     projectId: string,
     updateProjectDto: UpdateProjectDto,
   ): Promise<Project> {
-    const updatedProject = await this.projectModel.findByIdAndUpdate(
-      projectId,
-      updateProjectDto,
-      { new: true },
-    );
-    if (!updatedProject) {
-      throw new NotFoundException(projectConstants.NOT_FOUND);
+    try {
+      const updatedProject = await this.projectModel.findByIdAndUpdate(
+        projectId,
+        updateProjectDto,
+        { new: true },
+      );
+      if (!updatedProject) {
+        throw new NotFoundException(projectConstants.NOT_FOUND);
+      }
+      return updatedProject;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-    return updatedProject;
   }
 
   async deleteProject(profileId: string, projectId: string) {
-    const profile = await this.profileService.getProfileById(profileId);
-    if (!profile) {
-      throw new BadRequestException(profileConstants.BAD_REQUEST);
-    }
     try {
+      const profile = await this.profileService.getProfileById(profileId);
+      if (!profile) {
+        throw new BadRequestException(profileConstants.BAD_REQUEST);
+      }
       await this.projectModel.findByIdAndDelete(projectId);
     } catch (error) {
-      throw error;
+      throw new InternalServerErrorException(error.message);
     }
   }
 }
