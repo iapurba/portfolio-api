53a3430e0ca073b645b43600f2da2f30f27e6e8f
Apurba Panja <apurbapanja1@gmail.com>
2024-03-23 10:01:48 +0530

    Added auth error handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import {
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -18,45 +19,57 @@ export class AuthService {
   ) {}
 
   async login(loginDto: LoginDto) {
-    const user = await this.userService.findOneByEmail(loginDto.email);
-    if (!user) {
-      throw new NotFoundException('User does not exist');
+    try {
+      const user = await this.userService.findOneByEmail(loginDto.email);
+      if (!user) {
+        throw new NotFoundException('User does not exist');
+      }
+      const passwordMatch = await bcrypt.compare(
+        loginDto.password,
+        user.password,
+      );
+      if (!passwordMatch) {
+        throw new UnauthorizedException('Invalid Credentials');
+      }
+      const payload = { email: user.email, sub: user._id, role: user.role };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-    const passwordMatch = await bcrypt.compare(
-      loginDto.password,
-      user.password,
-    );
-    if (!passwordMatch) {
-      throw new UnauthorizedException('Invalid Credentials');
-    }
-    const payload = { email: user.email, sub: user._id, role: user.role };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
   }
 
   async signUp(signUpDto: SignUpDto) {
-    const hashedPassword = await bcrypt.hash(signUpDto.password, 10);
-    const createdUser = await this.userService.createUser({
-      email: signUpDto.email,
-      firstname: signUpDto.firstname,
-      lastname: signUpDto.lastname,
-      password: hashedPassword,
-      role: signUpDto.role,
-    });
-    return {
-      message: 'User Created Successful',
-      user: {
-        email: createdUser.email,
-        firstname: createdUser.firstname,
-        lastname: createdUser.lastname,
-        role: createdUser.role,
-      },
-    };
+    try {
+      const hashedPassword = await bcrypt.hash(signUpDto.password, 10);
+      const createdUser = await this.userService.createUser({
+        email: signUpDto.email,
+        firstname: signUpDto.firstname,
+        lastname: signUpDto.lastname,
+        password: hashedPassword,
+        role: signUpDto.role,
+      });
+      return {
+        message: 'User Created Successful',
+        user: {
+          email: createdUser.email,
+          firstname: createdUser.firstname,
+          lastname: createdUser.lastname,
+          role: createdUser.role,
+        },
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 
   async validateUser(payload: JwtPayload): Promise<any> {
-    const user = await this.userService.findOneByEmail(payload.email);
-    return user ? { email: user.email, id: user._id } : null;
+    try {
+      const user = await this.userService.findOneByEmail(payload.email);
+      return user ? { email: user.email, id: user._id } : null;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
@@ -10,15 +14,23 @@ export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const user = await this.userModel.findOne({ email: createUserDto.email });
-    if (user) {
-      throw new ConflictException(authConstants.USER_EXISTS);
+    try {
+      const user = await this.userModel.findOne({ email: createUserDto.email });
+      if (user) {
+        throw new ConflictException(authConstants.USER_EXISTS);
+      }
+      const createdUser = new this.userModel(createUserDto);
+      return await createdUser.save();
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
     }
-    const createdUser = new this.userModel(createUserDto);
-    return await createdUser.save();
   }
 
   async findOneByEmail(email: string): Promise<User | null> {
-    return this.userModel.findOne({ email }).exec();
+    try {
+      return this.userModel.findOne({ email }).exec();
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 }
